bb85c93c3a0bff83b6bb49a949e032fefe34b2bb
Andreas Gruhler <andreas.gruhler@adfinis.com>
2023-04-07 02:18:02 +0200

    doc: use vault namespace

--- Vault-Deployment/add-to-hosts.sh
+++ Vault-Deployment/add-to-hosts.sh
@@ -3,7 +3,7 @@
 # function that takes kubernetes ingress name and domain name and ands it to /etc/hosts
 
 function add_ingress_to_hosts() {
-    echo -e  "$(kubectl get ingress $1 -o jsonpath='{.status.loadBalancer.ingress[0].ip}') $2" | sudo tee -a /etc/hosts
+    echo -e  "$(kubectl get ingress $1 -n$2 -o jsonpath='{.status.loadBalancer.ingress[0].ip}') $3" | sudo tee -a /etc/hosts
 }
 
 
@@ -22,13 +22,13 @@ echo -e "### vault playground start ###" | sudo tee -a /etc/hosts
 echo -e "#the cleanup script will delete everything between this tags" | sudo tee -a /etc/hosts
 
 
-add_ingress_to_hosts "vault-ui-ingress" "vault-ui.playground.lab"
-add_ingress_to_hosts "vault-cli-ingress" "vault-cli.playground.lab"
-add_ingress_to_hosts "prometheus-ingress" "prometheus.playground.lab"
-add_ingress_to_hosts "grafana-ingress" "grafana.playground.lab"
-add_ingress_to_hosts "openldap-stack-ha-ltb-passwd" "ssl-ldap2.playground.lab"
-add_ingress_to_hosts "openldap-stack-ha-phpldapadmin" "phpldapadmin.playground.lab"
-add_ingress_to_hosts "homer" "explore.playground.lab"
+add_ingress_to_hosts "vault-ui-ingress" "vault" "vault-ui.playground.lab"
+add_ingress_to_hosts "vault-cli-ingress" "vault" "vault-cli.playground.lab"
+add_ingress_to_hosts "prometheus-ingress" "$VAULT_K8S_NAMESPACE" "prometheus.playground.lab"
+add_ingress_to_hosts "grafana-ingress" "$VAULT_K8S_NAMESPACE" "grafana.playground.lab"
+add_ingress_to_hosts "openldap-stack-ha-ltb-passwd" "$VAULT_K8S_NAMESPACE"  "ssl-ldap2.playground.lab"
+add_ingress_to_hosts "openldap-stack-ha-phpldapadmin" "$VAULT_K8S_NAMESPACE" "phpldapadmin.playground.lab"
+add_ingress_to_hosts "homer" "$VAULT_K8S_NAMESPACE" "explore.playground.lab"
 
 
 
