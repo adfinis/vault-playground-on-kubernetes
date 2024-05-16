8b259b8b6133e218005f0c34da80751bb8d352b7
Andreas Gruhler <andreas.gruhler@adfinis.com>
2024-05-16 09:45:56 +0200

    fix: dependency apply ingress in ns

--- Vault-Deployment/network-policies.tf
+++ Vault-Deployment/network-policies.tf
@@ -1,11 +1,13 @@
 # Apply Kubernetes network policies to harden tcp communication between namespaces
 resource "kubernetes_manifest" "kubernetes-networkpolicy-loki" {
-  count    = var.enable_kubernetes_network_policies ? 1 : 0
-  manifest = yamldecode(file("./manifests/network-policies/networkpolicy-loki-ingress.yaml"))
+  count      = var.enable_kubernetes_network_policies ? 1 : 0
+  manifest   = yamldecode(file("./manifests/network-policies/networkpolicy-loki-ingress.yaml"))
+  depends_on = [helm_release.loki]
 }
 resource "kubernetes_manifest" "kubernetes-networkpolicy-otlp" {
-  count    = var.enable_kubernetes_network_policies ? 1 : 0
-  manifest = yamldecode(file("./manifests/network-policies/networkpolicy-otlp-ingress.yaml"))
+  count      = var.enable_kubernetes_network_policies ? 1 : 0
+  manifest   = yamldecode(file("./manifests/network-policies/networkpolicy-otlp-ingress.yaml"))
+  depends_on = [helm_release.opentelemetry-collector]
 }
 
 # Apply Cilium network policies to harden tcp communication between namespaces
