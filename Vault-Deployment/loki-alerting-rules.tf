3fae95f00abaf38961cf35a0b5341e8c84fe7b88
Andreas Gruhler <andreas.gruhler@adfinis.com>
2024-05-17 13:23:45 +0200

    fix: separate loki from prom rules

--- Vault-Deployment/loki-alerting-rules.tf
+++ Vault-Deployment/loki-alerting-rules.tf
@@ -1,5 +1,11 @@
 # Loki alerting and recording rules
 resource "kubernetes_manifest" "loki-alerting-rules" {
-  manifest = yamldecode(file("./manifests/loki-alerting-rules/vault-audit-rules.yaml"))
+  manifest = yamldecode(file("./manifests/loki-alerting-rules/loki-rules.yaml"))
   depends_on = [helm_release.loki]
+}
+
+# Prometheus alerting and recording rules
+resource "kubernetes_manifest" "prometheus-alerting-rules" {
+  manifest = yamldecode(file("./manifests/loki-alerting-rules/prometheus-rules.yaml"))
+  depends_on = [helm_release.prometheus-grafana]
 }
